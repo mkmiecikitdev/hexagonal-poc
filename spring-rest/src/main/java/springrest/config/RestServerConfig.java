a6748e20a5638452b7bb2a7d53042d725ca703a4
M.Kmiecik <mich.kmiecik@gmail.com>
2021-01-15 20:50:08 +0100

    changed name of rest server config

--- spring-rest/src/main/java/springrest/config/RestServerConfig.java
+++ spring-rest/src/main/java/springrest/config/RestServerConfig.java
@@ -1,15 +1,15 @@
 package springrest.config;
 
-import jpapersistence.JpaConfig;
 import domain.registration.RegistrationFacade;
 import domain.registration.RegistrationRepository;
+import jpapersistence.JpaConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(JpaConfig.class)
-public class AppConfig {
+public class RestServerConfig {
     @Bean
     RegistrationFacade registrationFacade(final RegistrationRepository registrationRepository) {
         return new RegistrationFacade(registrationRepository);
